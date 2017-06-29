5d0729d9fcf891c9e65a60c25da4eda0ba5a1183
kasthurikaliappan <kasthuridivya97@gmail.com>
2017-06-29 12:01:14 +0530

    Add files via upload

---
+++ factorialnumber.java
@@ -0,0 +1,16 @@
+package gk;
+import java.util.*;
+public class factorialnumber {
+	public static void main(String args[]){
+		Scanner s=new Scanner(System.in);
+		int i,fact=5;
+		int n=1;
+		for(i=1;i<=n;i++){
+			fact=fact*i;
+			
+		}
+		System.out.println("factorial"+n+"is"+fact);
+				
+	}
+
+}
